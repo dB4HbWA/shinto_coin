3b745fd7dfa1ba969cdd8b2cebab11f2c78b1a55
Vince Carini <vincecarini@northwesternmutual.com>
2018-02-07 13:24:17 -0600

    nav bar working with no display of components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './ui-toolkit/css/nm-cx/main.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  withRouter // a higher order component
+} from 'react-router-dom'
+
+const NmTab = (props) => {
+  return (
+    <Route path={props.to} children={({match}) => {
+      return (
+        <li className={`tab-title ${match ? 'active' : ''}`} >
+          <Link to={props.to}>{props.tabName}</Link>
+        </li>
+      )}
+    } />
+  )
+}
+
+const ButtonGroup = props => (
+  <nav>
+    <ul className="tabs">
+      <NmTab to={"/home"} tabName="Home" />
+      <NmTab to={"/mine"} tabName="Mine Coins" />
+      <NmTab to={"/buy"} tabName="Buy Coins" />
+      <NmTab to={"/sell"} tabName="Sell Coins" />
+      <NmTab to={"/ledger"} tabName="Browse Ledger" />
+    </ul>
+  </nav>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <ButtonGroup />
+          {/* <Switch>
+            <Route path="/home" component={HomeView} />
+            <Route path="/mine" component={MineView} />
+            <Route path="/buy" component={BuyView} />
+            <Route path="/sell" component={SellView} />
+            <Route path="/ledger" component={LedgerView} />
+            <Route path="/transaction/:id" component={TransactionView} />
+          </Switch> */}
+        </div>
+      </Router>
     );
   }
 }
-
 export default App;
